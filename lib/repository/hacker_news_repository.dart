3bea534e0147c4bcd8108e8e5d9148efcec6210a
Funncy <gywns41@naver.com>
2022-04-24 12:58:15 +0900

    freezed 준비

--- lib/repository/hacker_news_repository.dart
+++ lib/repository/hacker_news_repository.dart
@@ -16,38 +16,4 @@ abstract class HackerNewsRepository {
 
   @GET('/item/{id}.json')
   Future<NewsModel> getNewsModelDetail(@Path() int id);
-
-  @GET("/NewsModels/{id}")
-  Future<NewsModel> getNewsModel(@Path("id") String id);
-
-  @GET('/demo')
-  Future<String> queries(@Queries() Map<String, dynamic> queries);
-
-  @GET("https://httpbin.org/get")
-  Future<String> namedExample(
-      @Query("apikey") String apiKey,
-      @Query("scope") String scope,
-      @Query("type") String type,
-      @Query("from") int from);
-
-  @PATCH("/NewsModels/{id}")
-  Future<NewsModel> updateNewsModelPart(
-      @Path() String id, @Body() Map<String, dynamic> map);
-
-  @PUT("/NewsModels/{id}")
-  Future<NewsModel> updateNewsModel(
-      @Path() String id, @Body() NewsModel NewsModel);
-
-  @DELETE("/NewsModels/{id}")
-  Future<void> deleteNewsModel(@Path() String id);
-
-  @POST("/NewsModels")
-  Future<NewsModel> createNewsModel(@Body() NewsModel NewsModel);
-
-  @POST("http://httpbin.org/post")
-  Future<void> createNewNewsModelFromFile(@Part() File file);
-
-  @POST("http://httpbin.org/post")
-  @FormUrlEncoded()
-  Future<String> postUrlEncodedFormData(@Field() String hello);
 }
